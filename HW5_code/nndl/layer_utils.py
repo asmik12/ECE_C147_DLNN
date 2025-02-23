9bc95b529912ab3397de57b76f9801c5926880a1
asmik12 <asmikawatkar12@g.ucla.edu>
2025-02-23 14:27:38 -0800

    Replaced optim and layer utils for HW 5

--- HW5_code/nndl/layer_utils.py
+++ HW5_code/nndl/layer_utils.py
@@ -36,3 +36,75 @@ def affine_relu_backward(dout, cache):
   da = relu_backward(dout, relu_cache)
   dx, dw, db = affine_backward(da, fc_cache)
   return dx, dw, db
+
+def affine_batchnorm_relu_forward(x, w, b, gamma, beta, bn_param):
+  '''
+  Forward pass for the affine-batchnorm-relu layers
+  '''
+
+  a, fc_cache = affine_forward(x, w, b)
+  bn, bn_cache = batchnorm_forward(a, gamma, beta, bn_param)
+  out, relu_cache = relu_forward(bn)
+  cache = (fc_cache, bn_cache, relu_cache)
+  return out, cache
+
+def affine_batchnorm_relu_backward(dout, cache):
+  '''
+  Backward pass for the affine-batchnorm-relu layers
+  '''
+  fc_cache, bn_cache, relu_cache = cache
+  da = relu_backward(dout, relu_cache)
+  d_bn, dgamma, dbeta = batchnorm_backward(da, bn_cache)
+  dx, dw, db = affine_backward(d_bn, fc_cache)
+  return dx, dw, db, dgamma, dbeta
+
+def affine_batchnorm_relu_dropout_forward(x,w,b, gamma, beta, bn_param, dropout_param=None):
+  '''
+  Forward pass for the affine-batchnorm-relu-dropout layers
+  '''
+  a, fc_cache = affine_forward(x, w, b)
+  bn, bn_cache = batchnorm_forward(a, gamma, beta, bn_param)
+  out, relu_cache = relu_forward(bn)
+  if dropout_param is not None:
+    bn_out = out.copy()
+    out, dropout_cache = dropout_forward(bn_out, dropout_param)
+    cache = (fc_cache, bn_cache, relu_cache, dropout_cache)
+ 
+  else:
+    cache = (fc_cache, bn_cache, relu_cache)
+
+  return out, cache
+
+
+def affine_batchnorm_relu_dropout_backward(dout, cache):
+  '''
+  Backward pass for the affine-batchnorm-relu layers
+  '''
+  fc_cache, bn_cache, relu_cache, dropout_cache = cache
+  dh = dropout_backward(dout, dropout_cache)
+  da = relu_backward(dh, relu_cache)
+  d_bn, dgamma, dbeta = batchnorm_backward(da, bn_cache)
+  dx, dw, db = affine_backward(d_bn, fc_cache)
+  
+  return dx, dw, db, dgamma, dbeta
+
+def affine_relu_dropout_forward(x, w, b, dropout_param=None):
+    a, fc_cache = affine_forward(x, w, b)
+    out, relu_cache = relu_forward(a)
+
+    if dropout_param is not None:
+      temp = out.copy()
+      out, dropout_cache = dropout_forward(temp, dropout_param)
+      cache = (fc_cache, relu_cache, dropout_cache)
+      
+    else: cache = (fc_cache, relu_cache)
+    return out, cache
+
+def affine_relu_dropout_backward(dout, cache):
+  fc_cache, relu_cache, dropout_cache = cache
+  
+  dh = dropout_backward(dout, dropout_cache)
+  da = relu_backward(dh, relu_cache)
+  dx, dw, db = affine_backward(da, fc_cache)
+  
+  return dx, dw, db
